e0bac45df32fedd4c30070585ee45c34fa1910ac
leesg619 <52132929+leesg619@users.noreply.github.com>
2021-07-27 21:12:37 +0900

    01문자찾기

---
+++ findString01.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class findString01 {
+    public static int solution(String str, char t) {
+        int answer= 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+        // System.out.println(str+" "+ t);
+        /*for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i)==t) answer++;
+        }*/
+        for(char x: str.toCharArray()) { //for-each는 String형그대로 불가!
+            if(x==t) answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+        kb.close();
+        System.out.println(solution(str, c));
+    }
+}
