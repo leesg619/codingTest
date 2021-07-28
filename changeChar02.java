7ea83e0187075fd7e3a8ec53d12682c786a49375
leesg619 <52132929+leesg619@users.noreply.github.com>
2021-07-28 17:39:24 +0900

    02문자변환

---
+++ changeChar02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class changeChar02 {
+    public static String solution(String str) {
+        String answer = "";
+        for(char x: str.toCharArray()) {
+            if(Character.isUpperCase(x)) {
+                answer+=Character.toLowerCase(x);
+            } else {
+                answer+=Character.toUpperCase(x);
+            }
+        }
+
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(solution(str));
+    }
+}
