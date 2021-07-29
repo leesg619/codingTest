b0afe52315f1a5f2340089ed2e7bb482fe47d452
leesg619 <52132929+leesg619@users.noreply.github.com>
2021-07-29 21:58:38 +0900

    05특정문자뒤집기

---
+++ WordFlip2_05.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class WordFlip2_05 {
+    public static String solution(String str) {
+        char[] arr = str.toCharArray();
+
+        int lt = 0, rt = str.length()-1;
+        while(lt<rt) {
+            if(!Character.isAlphabetic(arr[lt])) lt++;
+			else if(!Character.isAlphabetic(arr[rt])) rt--;
+            else {
+                char temp = arr[lt];
+                arr[lt] = arr[rt];
+                arr[rt] = temp;
+                lt += 1;
+                rt -= 1;
+            } 
+        }
+        String answer = String.valueOf(arr);
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(solution(str));
+    }
+}
