f06a9c905bdfb2a0c5820f3578f7a072dbf2458e
leesg619 <52132929+leesg619@users.noreply.github.com>
2021-07-30 19:55:16 +0900

    07회문문자열

--- WordCircular07.java
+++ WordCircular07.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 //거꾸로해도 같은문자열인지 판단
 public class WordCircular07 {
     public static String solution(String str) {
+        // String tmp = new StringBuilder(str).reverse().toString();
+        // if(str.equalsequalsIgnoreCase(tmp)) return "YES";
+        // else return "NO";
         int lt = 0;
         int rt = str.length() - 1;
         str = str.toLowerCase();
@@ -16,7 +19,7 @@ public class WordCircular07 {
     }
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        String str = kb.nextLine();
+        String str = kb.next();
         System.out.println(solution(str));
         kb.close();
     }
