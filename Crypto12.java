ce7db51f39aa93df3a0503cb28705bac3f172558
leesg619 <52132929+leesg619@users.noreply.github.com>
2021-08-04 19:19:27 +0900

    12암호

---
+++ Crypto12.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Crypto12 {
+    
+    public static String solution(int n, String str) {
+        String answer ="";
+        str = str.replace('#', '1').replace('*', '0');
+        for(int i = 0; i < str.length(); i = i+7) {
+            String sub = str.substring(i, i+7);
+            
+            char  chr = (char)Integer.parseInt(sub, 2);
+            answer += chr;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String str = kb.next();
+        System.out.println(solution(n, str));
+        kb.close();
+    }
+
+}
