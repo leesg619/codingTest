57d20ad8b20fc16b050d0ba445109a4f2e178e38
leesg619 <52132929+leesg619@users.noreply.github.com>
2021-08-10 17:45:41 +0900

    04피보나치

---
+++ Fibonacci04.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Fibonacci04 {
+    
+    public static ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(1);
+        answer.add(1);
+        for(int i = 0; i < n-2; i++) {
+            int a = answer.get(i);
+            int b = answer.get(i+1);
+            answer.add(a+b);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+		for(int x : solution(n)){
+			System.out.print(x+" ");
+        }
+        kb.close();
+    }
+
+}
