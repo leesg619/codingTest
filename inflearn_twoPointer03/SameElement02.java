334a289a891e0a1f0dee903aa51c364ce77aab74
leesg619 <52132929+leesg619@users.noreply.github.com>
2021-08-20 19:55:17 +0900

    02공통원소구하기

--- inflearn_twoPointer03/SameElement02.java
+++ inflearn_twoPointer03/SameElement02.java
@@ -4,8 +4,26 @@ import java.util.*;
 
 public class SameElement02 {
     
-    public static ArrayList<Integer> solution(int n, int[] arr) {
+    public static ArrayList<Integer> solution(int n, int[] arr1, int m, int[] arr2) {
         ArrayList<Integer> answer = new ArrayList<>();
+        int idx1 = 0;
+        int idx2 = 0;
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        while(idx1 < n && idx2 < m) {
+            if(arr1[idx1] == arr2[idx2]) {
+                answer.add(arr1[idx1]);
+                idx1 += 1;
+                idx2 += 1;
+            }
+            else if(arr1[idx1] > arr2[idx2]) {
+                idx2 += 1;
+            }
+            else if(arr1[idx1] < arr2[idx2]) {
+                idx1 += 1;
+            }
+        }
+        
         return answer;
 
     }
@@ -13,11 +31,18 @@ public class SameElement02 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int n=kb.nextInt();
-		int[] arr=new int[n];
+		int[] arr1=new int[n];
 		for(int i=0; i<n; i++){
-			arr[i]=kb.nextInt();
+			arr1[i]=kb.nextInt();
 		}
-		for(int x : solution(n, arr)){
+        
+        int m=kb.nextInt();
+		int[] arr2=new int[m];
+		for(int i=0; i<m; i++){
+			arr2[i]=kb.nextInt();
+		}
+
+		for(int x : solution(n, arr1, m, arr2)){
 			System.out.print(x+" ");
         }
         kb.close();
