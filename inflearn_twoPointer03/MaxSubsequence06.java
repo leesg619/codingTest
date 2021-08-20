aeda8056cce66aac216c4a5a1746a8e3c4da0d13
leesg619 <52132929+leesg619@users.noreply.github.com>
2021-08-20 17:00:04 +0900

    문제

---
+++ inflearn_twoPointer03/MaxSubsequence06.java
@@ -0,0 +1,26 @@
+package inflearn_twoPointer03;
+
+import java.util.*;
+
+public class MaxSubsequence06 {
+    
+    public static ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++){
+			arr[i]=kb.nextInt();
+		}
+		for(int x : solution(n, arr)){
+			System.out.print(x+" ");
+        }
+        kb.close();
+    }
+
+}
