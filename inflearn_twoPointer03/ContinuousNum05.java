6514687d46a630f2245058a635eda44bad784201
leesg619 <52132929+leesg619@users.noreply.github.com>
2021-08-23 17:58:03 +0900

    연속된자연수

--- inflearn_twoPointer03/ContinuousNum05.java
+++ inflearn_twoPointer03/ContinuousNum05.java
@@ -3,9 +3,23 @@ package inflearn_twoPointer03;
 import java.util.*;
 
 public class  ContinuousNum05 {
-    
-    public static ArrayList<Integer> solution(int n, int[] arr) {
-        ArrayList<Integer> answer = new ArrayList<>();
+    public static int solution(int n) {
+        int answer = 0;
+        int lt = 1, rt = 1;
+        int sum = 0;
+        while( rt <= (n/2)+2) {
+            if(sum < n) {
+                sum += rt;
+                rt += 1;
+            } else if(sum > n) {
+                sum -= lt;
+                lt += 1;
+            } else if(sum == n) {
+                answer += 1;
+                sum += rt;
+                rt += 1;
+            }
+        }
         return answer;
 
     }
@@ -13,13 +27,7 @@ public class  ContinuousNum05 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int n=kb.nextInt();
-        int[] arr=new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=kb.nextInt();
-        }
-        for(int x : solution(n, arr)){
-            System.out.print(x+" ");
-        }
+        System.out.print(solution(n));
         kb.close();
     }
 
