47b1b4ad944cd990beddea8ea2603bb53ec7577b
leesg619 <52132929+leesg619@users.noreply.github.com>
2021-08-30 18:10:52 +0900

    K번째큰수(틀)

--- Hash0405.java
+++ Hash0405.java
@@ -3,40 +3,22 @@ import java.util.*;
 public class Hash0405 {
     
     public static int solution(int n, int k, int[] arr) {
-        int answer = 0;
-        ArrayList<Integer> lists = new ArrayList<>();
-        for(int x: arr) lists.add(x);
-        lists.sort(Collections.reverseOrder());
-        ArrayList<Integer> results = new ArrayList<>();
-        HashMap<Integer, Integer> map= new HashMap<>();
+        int answer = -1;
+        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
 
         int lt = 0, mt = 1, rt = 2;
-        for(lt = 0; lt< n-2; lt++) {
-
-            for(mt = lt + 1; mt < n-1; mt++) {
-
+        for(lt = 0; lt< n; lt++) {  //n 까지로 해도 밑에서 조건안만족해서 for문 안돌아감
+            for(mt = lt + 1; mt < n; mt++) {
                 for(rt = mt + 1; rt < n; rt++) {
-                    int temp = arr[lt] + arr[mt] + arr[rt];
-                    results.add(temp);
+                    Tset.add(arr[lt] + arr[mt] + arr[rt]);
                 }
             }
         }
-
-        results.sort(Collections.reverseOrder());
-        for(int x: results) {
-            map.put(x, map.getOrDefault(x, 0)+1);
+        int cnt = 0;
+        for(int x: Tset) {
+            cnt++;
+            if(cnt == k ) return x;
         }
-        int temp = 0;
-        //키셋 잘 넣었고 이거 망했으니까 해쉬로 k 좀 계산해서 results로 리턴해보기
-
-        for(int x : map.keySet()) {
-            if(map.get(x) > 1) {
-                k += map.get(x)-1;
-            }
-        }
-
-        if(results.size() < k) return -1;
-        answer = results.get(k);
         return answer;
     }
 
