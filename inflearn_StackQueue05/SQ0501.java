c49f278fc7e5c7ca0646c5961658dbb968d9ecef
Lee SeokGeun <gkwoap@nate.com>
2021-10-04 20:33:00 +0900

    01올바른 괄호

--- inflearn_StackQueue05/SQ0501.java
+++ inflearn_StackQueue05/SQ0501.java
@@ -1,4 +1,29 @@
 package inflearn_StackQueue05;
+import java.util.*;
 
 public class SQ0501 {
+
+    public static String solution(String str) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+        for (char x : str.toCharArray()) {
+            if (x == '(') {
+                stack.push(x);
+            } else {
+                if (stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+        if (!stack.isEmpty()) { return "NO"; }
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str=kb.nextLine();
+        System.out.println(solution(str));
+        kb.close();
+    }
+
 }
