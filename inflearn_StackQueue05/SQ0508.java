ada11f37f75ef84de915694ab6d80a6687aa113f
leesg619 <gkwoap@nate.com>
2021-10-10 22:55:13 +0900

    Update SQ0508.java

--- inflearn_StackQueue05/SQ0508.java
+++ inflearn_StackQueue05/SQ0508.java
@@ -1,4 +1,28 @@
 package inflearn_StackQueue05;
 
 public class SQ0508 {
+
+    public static int solution(int n, int k, int[] arr) {
+        int aim = arr[k];
+        int now_index = k;
+        int max_index = n-1;
+        Queue<Character> q1 = new LinkedList<>();
+        for (int i : arr) q1.offer;
+
+
+        return 100;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=kb.nextInt();
+        }
+        System.out.print(solution(n, k, arr));
+        kb.close();
+    }
 }
