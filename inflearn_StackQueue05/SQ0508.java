a1a5cf24cd61709fd84493d5687a0469455bd88f
Lee SeokGeun <gkwoap@nate.com>
2021-10-11 16:49:16 +0900

    응급실

--- inflearn_StackQueue05/SQ0508.java
+++ inflearn_StackQueue05/SQ0508.java
@@ -1,17 +1,33 @@
 package inflearn_StackQueue05;
+import java.util.*;
 
 public class SQ0508 {
 
     public static int solution(int n, int k, int[] arr) {
-        int aim = arr[k];
-        int now_index = k;
-        int max_index = n-1;
-        Queue<Character> q1 = new LinkedList<>();
-        for (int i : arr) q1.offer;
+        int answer = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //해쉬로 위험도 저장하는 방법도있고 아예 Person 클래스 객체 생성해도됨
+        Queue<Integer> q = new LinkedList<>();
 
+        for (int i = 0; i < n; i++) map.put(i, arr[i]);
+        for (int i = 0; i < n; i++) q.offer(i);
 
-        return 100;
+        while (q.contains(k)) {
+            int temp = q.poll();
+            boolean ok = true;
 
+            for (int num : q) {
+                if (map.get(temp) < map.get(num)) {
+                    q.offer(temp);
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (ok) answer++;
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
@@ -22,7 +38,7 @@ public class SQ0508 {
         for(int i=0; i<n; i++){
             arr[i]=kb.nextInt();
         }
-        System.out.print(solution(n, k, arr));
+        System.out.print(solution(n, m, arr));
         kb.close();
     }
 }
