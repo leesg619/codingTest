dfbcdde7ca5b9f20255991222693a6cb81cf32d6
leesg619 <gkwoap@nate.com>
2021-10-19 16:51:29 +0900

    중복확인

--- inflearn_Sort06/Sort0605.java
+++ inflearn_Sort06/Sort0605.java
@@ -1,4 +1,23 @@
 package inflearn_Sort06;
+import java.util.*;
 
 public class Sort0605 {
+    public static String solution(int n, int[] arr){
+        String answer = "U";
+        Arrays.sort(arr);
+        for(int i=0; i<n-1; i++){
+            if(arr[i]==arr[i+1]){
+                answer="D";
+                break;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+        System.out.println(solution(n, arr));
+    }
 }
