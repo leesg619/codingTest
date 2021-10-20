0b2c981e4be44fdc089f8c3cc9859b1caa5bf209
Lee SeokGeun <gkwoap@nate.com>
2021-10-20 23:52:23 +0900

    장난꾸러기1

--- inflearn_Sort06/Sort0606.java
+++ inflearn_Sort06/Sort0606.java
@@ -1,4 +1,18 @@
 package inflearn_Sort06;
 
+import java.util.*;
+
 public class Sort0606 {
+    public ArrayList<Integer> solution(int n, int[] arr){
+
+
+
+    }
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+        for(int x : solution(n, arr)) System.out.print(x+" ");
+    }
 }
