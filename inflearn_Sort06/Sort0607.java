e6ba536f1eda74e7f48b33425d856609b30d0bd3
Lee SeokGeun <gkwoap@nate.com>
2021-10-27 20:27:08 +0900

    좌표 정렬(Comparable interface)

--- inflearn_Sort06/Sort0607.java
+++ inflearn_Sort06/Sort0607.java
@@ -1,4 +1,35 @@
 package inflearn_Sort06;
+import java.util.*;
 
 public class Sort0607 {
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        ArrayList<Point> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x = kb.nextInt();
+            int y = kb.nextInt();
+            arr.add(new Point(x, y));
+        }
+        Collections.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr.get(i).x +" "+ arr.get(i).y);
+        }
+    }
+}
+
+class Point implements Comparable<Point>{
+    int x,y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.x==o.x) return this.y-o.y;  // 오름차순 원하면 this - o 가 음수가 되게 설계하면된다.
+        else return this.x-o.x;
+    }
 }
