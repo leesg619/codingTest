0d713da38fb4c0bbd05d2fd1bd2204013f958db0
leesg619 <gkwoap@nate.com>
2021-11-07 23:35:11 +0900

    말단노드까지최소거리dfs

--- inflearn_Search07/TreeDFS.java
+++ inflearn_Search07/TreeDFS.java
@@ -1,13 +1,15 @@
 package inflearn_Search07;
 
-
+//말단노드까지최단경로를 구하는 것이기 때문에 dfs보다 bfs로 해야 되지만 연습용임. (else 호출시 root.lt, root.rt 호출하므로 트리는 완전해야함.rt나 lt 둘중하나 없는구조면 안됨)
 public class TreeDFS {
     Node root;
-    Node endNode;
-    int shortest;
-    // 말단까지의 최단경로
-    public void dfs(Node root) {
 
+    public int dfs(int L, Node root) {
+        if (root.lt == null && root.rt == null) {
+            return L;
+        } else {
+            return Math.min(dfs(L+1, root.lt), dfs(L+1, root.rt));
+        }
     }
 
     public static void main(String[] args) {
@@ -17,6 +19,7 @@ public class TreeDFS {
         tree.root.rt = new Node(3);
         tree.root.lt.lt = new Node(4);
         tree.root.lt.rt = new Node(5);
-        tree.dfs(tree.root);
+        int shortest = tree.dfs(0, tree.root);
+        System.out.println(shortest);
     }
 }
