6a7e19aa66a7937ba1a6202c3f1469ca8dbdccaf
leesg619 <gkwoap@nate.com>
2022-01-30 23:51:43 +0900

    섬나라DFS

--- inflearn_DFSBFS08/Island13.java
+++ inflearn_DFSBFS08/Island13.java
@@ -2,21 +2,21 @@ package inflearn_DFSBFS08;
 
 import java.util.Scanner;
 
-public class Island13 {
+public class Main {
     static int n, m;
-    static int[] land, ch;
+    static int[][] land;
     static int answer;
     static int[] dx={-1,0,1,0,-1,1,-1,1};
     static int[] dy={0,1,0,-1,-1,-1,1,1};
     static boolean flag = false;
 
     public void dfs(int x, int y) {
-        if ( x<=0 || y<=0 || x>=n || y>=n || land[x][y] == 0) return;
+        if ( x<0 || y<0 || x>=n || y>=n || land[x][y] == 0) return;
 
         if (land[x][y] == 1) {
             land[x][y] = 0; //check
             for (int i = 0; i < 8; i++) {
-                dfs(dx[i], dy[i]);
+                dfs(x+dx[i], y+ dy[i]);
             }
             flag = true;
         }
@@ -24,18 +24,17 @@ public class Island13 {
     }
 
     public static void main(String[] args) {
-        Island13 T = new Island13();
+        Main T = new Main();
         Scanner kb = new Scanner(System.in);
         n=kb.nextInt();
-
         land = new int[n][n];
-        ch = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 land[i][j] = kb.nextInt();
             }
         }
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 T.dfs(i, j);
@@ -47,6 +46,5 @@ public class Island13 {
             }
         }
 
-        System.out.println(answer);
     }
 }
